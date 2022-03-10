415dc645c6718717a6a541dbd3ab34c6a1aa0e80
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2022-03-10 20:10:29 +0530

    Add files via upload

---
+++ Uploading Images to Database/src/index.js
@@ -0,0 +1,23 @@
+let fileName = "";
+/*function print(){
+    console.log(fileName);
+}*/
+$(document).ready(function(){
+    $('#file_upload').change(function(e){
+        fileName = e.target.files[0].name;
+    });
+});
+$(document).ready(() => {
+    $("#file_upload").change(function () {
+        const file = this.files[0];
+        if (file) {
+            let reader = new FileReader();
+            document.getElementById("upload").style.display = "block";
+            reader.onload = function (event) {
+                $("#imgPreview")
+                  .attr("src", event.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+});
